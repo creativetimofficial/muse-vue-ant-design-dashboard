cbfed502c020f1a6f105251d49e6a244e9d3d7b7
dragos <dragos@creative-tim.com>
2021-07-21 18:41:58 +0300

    refactor: config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	runtimeCompiler: true,
-	publicPath: '/muse-vue-ant-design/',
+	publicPath: '/muse-vue-ant-design-dashboard/',
 	chainWebpack: config => {
 		config
 			.plugin('html')
 			.tap(args => {
-				args[0].title = 'Muse Vue Ant Design - by Creative Tim'
+				args[0].title = 'Muse Vue Ant Design Dashboard - by Creative Tim'
 				return args
 			})
 	}
