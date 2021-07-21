8438a2c6c6e451d22176bdca0668365bd255af45
dragos <dragos@creative-tim.com>
2021-07-21 17:35:48 +0300

    refactor: link

--- src/components/Sidebars/DashboardSidebar.vue
+++ src/components/Sidebars/DashboardSidebar.vue
@@ -122,7 +122,7 @@
 					</span>
 					<h6>Need Help?</h6>
 					<p>Please check our docs</p>
-					<a-button type="primary" block size="small">
+					<a-button type="primary" href="https://demos.creative-tim.com/muse-vue-ant-design/documentation" block target="_blank">
 						DOCUMENTATION
 					</a-button>
 				</div>
